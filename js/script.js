cc753ec96064c14fe1dce2790992a6e597a93b66
Arkadiusz Zygan <a.zygan@gmail.com>
2020-06-17 13:57:15 +0200

    Add fnc ready

--- js/script.js
+++ js/script.js
@@ -1,18 +1,26 @@
-console.log('start js');
-
-const removeCartItemButtons = document.querySelectorAll('.cartProductRow .removeBtn');
-
-for (let i = 0; i < removeCartItemButtons.length; i++) {
-  const removeButton = removeCartItemButtons[i];
-
-  removeButton.addEventListener('click', (event) => {
-    const buttonClicked = event.target;
-    buttonClicked.parentElement.parentElement.parentElement.remove();
-
-    updateCartTotal();
-  });
+if (document.readyState == 'loading') {
+  document.addEventListener('DOMContentLoaded', ready);
+} else {
+  ready()
 }
 
+
+function ready() {
+
+  const removeCartItemButtons = document.querySelectorAll('.cartProductRow .removeBtn');
+
+  for (let i = 0; i < removeCartItemButtons.length; i++) {
+    const removeButton = removeCartItemButtons[i];
+
+    removeButton.addEventListener('click', removeCartItem);
+  }
+}
+
+function removeCartItem(event) {
+  const buttonClicked = event.target;
+  buttonClicked.parentElement.parentElement.parentElement.remove();
+  updateCartTotal();
+}
 
 function updateCartTotal() {
 
