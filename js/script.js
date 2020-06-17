5bb23b1a28cc3fddb007a68322645934cc908f01
Arkadiusz Zygan <a.zygan@gmail.com>
2020-06-17 13:49:24 +0200

    Add updateCartTotal

--- js/script.js
+++ js/script.js
@@ -8,5 +8,29 @@ for (let i = 0; i < removeCartItemButtons.length; i++) {
   removeButton.addEventListener('click', (event) => {
     const buttonClicked = event.target;
     buttonClicked.parentElement.parentElement.parentElement.remove();
+
+    updateCartTotal();
   });
 }
+
+
+function updateCartTotal() {
+
+  const cartContainer = document.querySelector('.cartProductsContainer');
+  const cartProductRows = cartContainer.querySelectorAll('.cartProductRow');
+
+  let total = 0;
+
+  for (let i = 0; i < cartProductRows.length; i++) {
+    const cartProductRow = cartProductRows[i];
+    const priceEl = cartProductRow.querySelector('.cartRowPrice');
+    const quantityEl = cartProductRow.querySelector('.cartRowQuantity input');
+    const price = parseFloat(priceEl.innerText.replace(' $', ''));
+    const quantity = parseInt(quantityEl.value);
+    total = total + (price * quantity);
+  }
+
+  document.querySelector('.totalPrice').innerText = total + '$';
+}
+
+// updateCartTotal()
