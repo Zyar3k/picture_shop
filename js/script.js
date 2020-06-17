a35997d065ab6efcd5d8e7d8bf32a4b0732dada7
Arkadiusz Zygan <a.zygan@gmail.com>
2020-06-17 12:53:46 +0200

    Add removecart functionality

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 console.log('start js');
+
+const removeCartItemButtons = document.querySelectorAll('.cartProductRow .removeBtn');
+
+for (let i = 0; i < removeCartItemButtons.length; i++) {
+  const removeButton = removeCartItemButtons[i];
+
+  removeButton.addEventListener('click', (event) => {
+    const buttonClicked = event.target;
+    buttonClicked.parentElement.parentElement.parentElement.remove();
+  });
+}
