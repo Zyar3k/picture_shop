dca840d09fd5087c2e78813223e91ad8a06d4684
Arkadiusz Zygan <a.zygan@gmail.com>
2020-06-23 20:33:12 +0200

    Clean files structure

--- js/about.js
+++ js/about.js
@@ -1,18 +1,15 @@
-// console.log('about start');
-
-
 const boxTitles = document.querySelectorAll('.boxTitle');
 boxTitles.forEach(boxTitle => {
   boxTitle.addEventListener('click', () => {
-    boxTitle.parentElement.classList.add('grow')
-  })
+    boxTitle.parentElement.classList.add('grow');
+  });
 });
 
 const closeBtns = document.querySelectorAll('.closeBtn');
 closeBtns.forEach(closeBtn => {
   closeBtn.addEventListener('click', () => {
-    closeBtn.parentElement.classList.remove('grow')
-  })
+    closeBtn.parentElement.classList.remove('grow');
+  });
 });
 
 const showMoreBtns = document.querySelectorAll('.showMoreBtn');
@@ -20,50 +17,41 @@ const showMoreBtns = document.querySelectorAll('.showMoreBtn');
 showMoreBtns.forEach(showMoreBtn => {
   
   showMoreBtn.addEventListener('click', () => {
-    const id = (showMoreBtn.href).slice(33)
-
+    const id = (showMoreBtn.href).slice(33);
 
     const aboutDetails = document.querySelectorAll('.aboutDetails');
 
     aboutDetails.forEach(aboutDetail => {
 
-      
       if (id === aboutDetail.id) {
-        console.log(id)      
-        console.log(aboutDetail.id)
-        aboutDetail.classList.add('show')
+        aboutDetail.classList.add('show');
       }
     });
-
-    console.log(id)
-
-  })
+  });
 });
 
-
 // modals
-
 const overlay = document.querySelector('.overlay');
 const imagesGallery = document.querySelectorAll('.open');
 
 imagesGallery.forEach(image => {
 
   image.addEventListener('click', () => {
-    const src = image.querySelector('img').src; //src from category
+    const src = image.querySelector('img').src;
 
     const modal = document.createElement('div');
-    modal.classList.add('modal', 'open')
+    modal.classList.add('modal', 'open');
     const modalContent = `<img src="${src}" alt="">
     <img src="/images/closeBtn.png" class="close-modal" />`;
     modal.innerHTML = modalContent;
     overlay.append(modal);
-    overlay.classList.add('open')
+    overlay.classList.add('open');
 
     const closeModal = document.querySelector('.close-modal');
 
     closeModal.addEventListener('click', () => {
-      closeModal.parentElement.remove()
-      overlay.classList.remove('open')
+      closeModal.parentElement.remove();
+      overlay.classList.remove('open');
     });
-  })
+  });
 });
